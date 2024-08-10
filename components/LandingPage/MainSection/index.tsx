d443f8f584e9db662b8b3cf6c92746e2d0466f07
Daniel Guglielmi <dguglielmi.asm@gmail.com>
2024-08-10 14:19:40 -0300

    modify headers font sizes and color of the main page

--- components/LandingPage/MainSection/index.tsx
+++ components/LandingPage/MainSection/index.tsx
@@ -7,18 +7,18 @@ const MainSection = () => {
     <section className={Styles['mainsection-container']}>
       <div className={Styles['mainsection-container__signup']}>
         <span>
-          <h1 className={roboto500.className}>Join the Vibrant Snappy Community:</h1>
+          <h1 className={roboto500.className}>Join the Snappy Community</h1>
         </span>
 
         <span>
-          <h1 className={roboto500.className}>Your Gateway to Business Success</h1>
+          <h2 className={roboto500.className}>your gateway to Business Success...</h2>
         </span>
         <div className={Styles['mainsection-container__signup-text']}>
           <span>
-            <h3 className={roboto500.className}>Empower Your Business with Snappy:</h3>
+            <h3 className={roboto500.className}>Empower Your Business with Snappy</h3>
           </span>
           <span>
-            <h3 className={roboto500.className}>Streamlined Control for Smart Entrepreneurs</h3>
+            <h4 className={roboto500.className}>Streamlined Control for Smart Entrepreneurs</h4>
           </span>
         </div>
         <div className={Styles['mainsection-container__signup-signup']}>
