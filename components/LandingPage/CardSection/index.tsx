34a47b070c48a0569d45892099612680a5d35a3d
Daniel Guglielmi <dguglielmi.asm@gmail.com>
2024-08-11 00:03:53 -0300

    fixing translation from objects by using useTranslation to capture the status changes

--- components/LandingPage/CardSection/index.tsx
+++ components/LandingPage/CardSection/index.tsx
@@ -1,10 +1,71 @@
 import React from 'react';
 import SnappyCard from './SnappyCard';
-import { cards } from './cardList';
+import { mdiHandshakeOutline, mdiCartVariant, mdiBasket, mdiFactory, mdiAccountGroup, mdiReceiptText } from '@mdi/js';
+import { SnappyCardProps } from '@/types/SnappyCardProps';
 import Styles from './CardSection.module.scss';
 import Waves from '@/components/Waves';
+import { useTranslation } from 'react-i18next';
 
 const CardSection = () => {
+  const { i18n } = useTranslation();
+
+  const cards: SnappyCardProps[] = [
+    {
+      icon: mdiHandshakeOutline,
+      iconTitle: 'Business',
+      iconSize: 2,
+      iconColor: '#c985f7',
+      title: i18n.t('snappyCardBusinessTitle'),
+      content: i18n.t('snappyCardBusinessContent'),
+      learnmore: '#Business & Branch Offices',
+    },
+    {
+      icon: mdiBasket,
+      iconTitle: 'Product',
+      iconSize: 2,
+      iconColor: '#c985f7',
+      title: i18n.t('snappyCardProductTitle'),
+      content: i18n.t('snappyCardProductContent'),
+      learnmore: '#Product Management',
+    },
+    {
+      icon: mdiFactory,
+      iconTitle: 'Material',
+      iconSize: 2,
+      iconColor: '#c985f7',
+      title: i18n.t('snappyCardMaterialTitle'),
+      content: i18n.t('snappyCardMaterialContent'),
+      learnmore: '#Material Management',
+    },
+    {
+      icon: mdiAccountGroup,
+      iconTitle: 'Client',
+      iconSize: 2,
+      iconColor: '#c985f7',
+      title: i18n.t('snappyCardClientTitle'),
+      content: i18n.t('snappyCardClientContent'),
+      learnmore: '#Client Management',
+    },
+    {
+      icon: mdiReceiptText,
+      iconTitle: 'Invoicing',
+      iconSize: 2,
+      iconColor: '#c985f7',
+      title: i18n.t('snappyCardInvoicingTitle'),
+      content: i18n.t('snappyCardInvoicingContent'),
+      learnmore: '#Invoicing & Tax Management',
+    },
+    {
+      icon: mdiCartVariant,
+      iconTitle: 'Sales',
+      iconSize: 2,
+      iconColor: '#c985f7',
+      title: i18n.t('snappyCardSalesTitle'),
+      content: i18n.t('snappyCardSalesContent'),
+      learnmore: '#Sales, Purchases & Inventory Management',
+    },
+  ];
+
   return (
     <section className={Styles['cardsection-container']}>
       <Waves />
--- components/LandingPage/Descriptions/index.tsx
+++ components/LandingPage/Descriptions/index.tsx
@@ -1,48 +1,50 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import ContainerDesc from './components/ContainerDesc';
 import { isOdd } from '@/utils/util';
-import i18n from '@/locales/i18n';
-
-const descriptions = [
-  {
-    id: 1,
-    title: i18n.t('snappyServiceDescriptionBranchOffices'),
-    content: i18n.t('snappyServiceDescriptionBranchOfficesContent'),
-    image: '/stores.jpg',
-  },
-  {
-    id: 2,
-    title: i18n.t('snappyServiceDescriptionProductManagement'),
-    content: i18n.t('snappyServiceDescriptionProductManagementContent'),
-    image: '/products.png',
-  },
-  {
-    id: 3,
-    title: i18n.t('snappyServiceDescriptionMaterialManagement'),
-    content: i18n.t('snappyServiceDescriptionMaterialManagementContent'),
-    image: '/material.png',
-  },
-  {
-    id: 4,
-    title: i18n.t('snappyServiceDescriptionClientManagement'),
-    content: i18n.t('snappyServiceDescriptionClientManagementContent'),
-    image: '/clients.png',
-  },
-  {
-    id: 5,
-    title: i18n.t('snappyServiceDescriptionInvoicing'),
-    content: i18n.t('snappyServiceDescriptionInvoicingContent'),
-    image: '/taxes.png',
-  },
-  {
-    id: 6,
-    title: i18n.t('snappyServiceDescriptionSales'),
-    content: i18n.t('snappyServiceDescriptionSalesContent'),
-    image: '/sales.png',
-  },
-];
+import { useTranslation } from 'react-i18next';
 
 const Descriptions = () => {
+  const { i18n } = useTranslation();
+
+  const descriptions = [
+    {
+      id: 1,
+      title: i18n.t('snappyServiceDescriptionBranchOffices'),
+      content: i18n.t('snappyServiceDescriptionBranchOfficesContent'),
+      image: '/stores.jpg',
+    },
+    {
+      id: 2,
+      title: i18n.t('snappyServiceDescriptionProductManagement'),
+      content: i18n.t('snappyServiceDescriptionProductManagementContent'),
+      image: '/products.png',
+    },
+    {
+      id: 3,
+      title: i18n.t('snappyServiceDescriptionMaterialManagement'),
+      content: i18n.t('snappyServiceDescriptionMaterialManagementContent'),
+      image: '/material.png',
+    },
+    {
+      id: 4,
+      title: i18n.t('snappyServiceDescriptionClientManagement'),
+      content: i18n.t('snappyServiceDescriptionClientManagementContent'),
+      image: '/clients.png',
+    },
+    {
+      id: 5,
+      title: i18n.t('snappyServiceDescriptionInvoicing'),
+      content: i18n.t('snappyServiceDescriptionInvoicingContent'),
+      image: '/taxes.png',
+    },
+    {
+      id: 6,
+      title: i18n.t('snappyServiceDescriptionSales'),
+      content: i18n.t('snappyServiceDescriptionSalesContent'),
+      image: '/sales.png',
+    },
+  ];
+
   return (
     <>
       {descriptions.map((description) =>
